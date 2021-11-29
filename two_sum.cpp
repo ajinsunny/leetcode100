e8d6674c7ab85a908293597aa5d763365d17a344
Ajin Sunny <ajin.sunny@gmail.com>
2021-11-28 22:18:19 -0500

    (add) new solution added

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,3 +1,7 @@
+#include <vector>
+#include <map>
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -30,5 +34,29 @@ public:
             
         }
         return result;
+    }
+};
+
+class Solution2 {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        
+        map<int,int> map;
+        vector<int> pairs;
+        
+        for(int i = 0; i<nums.size(); i++){
+            int complement = target - nums[i];
+            if (map.find(complement)!=map.end()){
+                pairs.push_back(map.find(complement)->second);
+                pairs.push_back(i);
+                break;
+                
+            }
+            map.insert(pair<int, int>(nums[i],i));
+                
+        }
+        return pairs;
+        
     }
 };
